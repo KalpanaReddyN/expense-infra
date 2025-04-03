c89510feb64bb34c59d82bdfeb11bc60605bbcd9
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2025-04-03 20:27:06 +0100

    jenkins

---
+++ 20-bastion/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.66.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "81s-remote-state-development"
+    key    = "expense-bastion"
+    region = "us-east-1"
+    dynamodb_table = "81s-locking-development"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
---
+++ 40-eks/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.83.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "81s-remote-state-development"
+    key    = "expense-eks"
+    region = "us-east-1"
+    dynamodb_table = "81s-locking-development"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
